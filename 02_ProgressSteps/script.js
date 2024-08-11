73c850768741efdb5fb28ac989d0d723145973f3
YMarianaAguilar <ymarianaaguilar@gmail.com>
2024-08-11 11:37:01 -0300

    Se agrega Progress Steps

---
+++ 02_ProgressSteps/script.js
@@ -0,0 +1,56 @@
+//Selecciono elementos del DOM
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+//Establecezco una variable de Estado:
+//Mantiene un seguimiento del círculo "activo"
+//Comienza en 1, lo que significa que el primer círculo es el activo al inicio
+let currentActive = 1
+
+const update = () => {
+    circles.forEach((circle, index) => {
+        /*if (index < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }*/
+        
+        (index < currentActive)
+            ? circle.classList.add('active')
+            : circle.classList.remove('active')
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+    if (currentActive === 1) {
+        prev.disabled = true
+    } else if (currentActive === circles.length) {
+        next.desabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false}
+}
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
+})
